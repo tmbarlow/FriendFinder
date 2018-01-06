736b99ff73350bb0bf50e5b463bbc46ba70b225e
tmbarlow <tmbarlow@gmail.com>
2018-01-05 23:01:42 -0700

    updating existing files

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+        name: "tyler", 
+        photo: 'https://a4-images.myspacecdn.com/images03/34/2b4dadd56c4943ba9ae284174e05d145/300x300.jpg', 
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }, 
+    {
+        name: "Taylor",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Taylor_Swift_%286966830273%29.jpg",
+        scores: [
+            "5"
+        ]
+    }, 
+    {
+        name: "dwayne",
+        photo: "http://projectrockofficial.com/images/overlay_rock.jpg",
+        scores: [
+            "5"
+        ]
+    }, 
+    {
+        name: "Betty",
+        photo: "http://cdn.cnn.com/cnnnext/dam/assets/120904131250-betty-white-2012-full-169.jpg",
+        scores: [
+            "5"
+        ]
+    }
+]; 
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,61 @@
+var friends = require("../data/friends.js");
+// =============================================================
+// ROUTING
+// =============================================================
+
+module.exports = function(app) {
+  // API GET Requests
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  // API POST Requests
+  // --------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    
+    var bestMatch = {
+        name: "", 
+        photo: "", 
+        friendDifference: 1000
+    }; 
+    console.log (req.body);
+
+    //here we take the result of the user's survey post and parse it
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    console.log(userScores);
+    
+    var totalDifference = 0;
+
+    //Here we loop through all the friend possibilities in the database
+    for (var i=0; i< friends.length; i++) {
+        console.log(friends[i]);
+        totalDifference = 0;
+
+        //we then looop through all the scores of each friend
+        for (var j =  0; j < friends[i]; j++) {
+            
+            //calculate the difference between the scores and sum them into the total difference
+            totalDifference += Math.abs(parseInt(userScores[j])- parseInt(friends[i].scores[j]));
+            
+            //if the sum of the difference is less then the differences of the current "best match"
+            if (totalDifference <= bestMatch.friendDifference) {
+
+                //Reset the bestMatch to bet the new friend.
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+    }
+
+    //finally save the user's data to the database 
+    friends.push(userData);
+
+    //return a json with the users's bestMatch. this will be used by the html in the next page
+    res.json(bestMatch);
+  });
+};
